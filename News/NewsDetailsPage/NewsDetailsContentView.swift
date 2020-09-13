967e776383d43e09ae4a4e669d2d3891f63a08c0
alterecho <com.vjc@gmail.com>
2020-09-14 00:10:06 +0800

    added details page

---
+++ News/NewsDetailsPage/NewsDetailsContentView.swift
@@ -0,0 +1,15 @@
+//
+//  NewsDetailsView.swift
+//  News
+//
+//  Created by v.a.jayachandran on 26/4/20.
+//  Copyright © 2020 v.a.jayachandran. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewsDetailsContentView: View {
+    var body: some View {
+        Text("details")
+    }
+}
